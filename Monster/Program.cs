3df6084316ae27c904a0972057f756ee39489c20
Logan Madej <lnmadej23@gmail.com>
2021-08-30 10:41:53 -0400

    Add project files.

---
+++ Monster/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleCreatures
+{
+    class Program
+    {
+        static void Main()
+        {
+            // this program will create onsole creatures
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| O |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("Steve");
+        }
+    }
+}
